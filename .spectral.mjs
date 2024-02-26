600691622c7f669fb10f57c8cd7bfb05f330fd9c
ken.mcdonald@checkmarx.com <ken.mcdonald@checkmarx.com>
2024-02-26 17:45:21 -0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNzk2Mw/spectral.js?branch_slug=ken-mcdonald-added-My-API&token=29024638-d705-4478-8c0c-ef92aeac51ad";
+export default { extends: ruleset };
